5ee0a2e18900f6663c7742afa34fc4afe7e936b0
Steve Hall <sthall@microsoft.com>
2019-12-11 12:43:35 -0500

    modified to use shared libaries

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
+  preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
