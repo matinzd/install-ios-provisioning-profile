1cb2a2553ccc4ec3e1deb926e54b9b83b08564c0
Steve Hall <sthall@microsoft.com>
2019-12-11 14:12:24 -0500

    fixed tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -4,32 +4,24 @@ const exec = require('@actions/exec');
 import ios = require('../src/installprovprofile');
 import fs = require('fs');
 
-test('Install provisioning profile', async() => {
-    fs.readFile(path.join(__dirname, "testprovprofile.mobileprovision.base64"), "utf8", async (err, data) => {
-
-        //Simulates file being stored as a GitHub secret
-        core.exportVariable('INPUT_ENCODED-PROFILE', data);
-
-        await ios.installProvisioningProfileTask(); 
-      });
-});
-
-
-test('Check if provisioning profile installed correctly', async() => {
-  
+beforeAll(() => {
   fs.readFile(path.join(__dirname, "testprovprofile.mobileprovision.base64"), "utf8", async (err, data) => {
 
     //Simulates file being stored as a GitHub secret
     core.exportVariable('INPUT_ENCODED-PROFILE', data);
 
     await ios.installProvisioningProfileTask(); 
+  });
+});
 
-    let uuid = core.getInput('APPLE_PROV_PROFILE_UUID');
+
+test('Check if provisioning profile installed correctly', async() => {
+  
+  let uuid = core.getInput('APPLE_PROV_PROFILE_UUID');
     let altUuid = core.getInput('provisioningProfileUuid');
     let name = core.getInput('provisioningProfileName');
 
     console.log("UUID: " + uuid);
     console.log("Alt UUID: " + altUuid);
     console.log("Name: " + name);
-  });
 });
